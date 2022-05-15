3e630d86644eb4345c561c24f5bf872582b128c4
Yaroslav Paroshin <qzarov@mail.ru>
2022-05-15 22:45:09 +0300

    Clear code

--- Buttons-bot/deScienz-bot.py
+++ Buttons-bot/deScienz-bot.py
@@ -15,94 +15,85 @@ def start(message):
                          message.from_user), reply_markup=markup)
 
 
+def set_buttonz(btn1, btn2="", btn3=""):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    if btn2 == "":
+        markup.add(btn1)
+    elif btn3 == "":
+        markup.add(btn1, btn2)
+    else:
+        markup.add(btn1, btn2, btn3)
+
+    return markup
+
+
 @bot.message_handler(content_types=['text'])
 def func(message):
     if message.text == "Надо написать статью..." \
             or message.text == "Ну мааам..." \
             or message.text == "Ладно, иду делать..."\
             or message.text == "Пройти мимо":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Прокрастинировать...")
-        btn2 = types.KeyboardButton("Искать материалы")
-        markup.add(btn1, btn2)
-        bot.send_message(message.chat.id, text="Что будешь делать?)", reply_markup=markup)
+        bot.send_message(message.chat.id,
+                         text="Что будешь делать?)",
+                         reply_markup=set_buttonz("Прокрастинировать...", "Искать материалы"))
+
     elif message.text == "Прокрастинировать...":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("5 минут")
-        btn2 = types.KeyboardButton("1 час")
-        btn3 = types.KeyboardButton("Начну завтра)")
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, text="И сколько будем прокрастинировать?", reply_markup=markup)
+        bot.send_message(message.chat.id,
+                         text="И сколько будем прокрастинировать?",
+                         reply_markup=set_buttonz("5 минут", "1 час", "Начну завтра)"))
 
     elif message.text == "5 минут" \
             or message.text == "1 час" \
             or message.text == "Начну завтра)":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Ну мааам...")
-        btn2 = types.KeyboardButton("Ладно, иду делать...")
-        markup.add(btn1, btn2)
-        bot.send_message(message.chat.id, text="А чего не с понедельника?", reply_markup=markup)
+        bot.send_message(message.chat.id,
+                         text="А чего не с понедельника?",
+                         reply_markup=set_buttonz("Ну мааам...", "Ладно, иду делать..."))
 
     elif message.text == "Искать материалы":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("Платные базы")
-        button2 = types.KeyboardButton("Интернет")
-        markup.add(button1, button2)
-        bot.send_message(message.chat.id, text="Где будем искать?", reply_markup=markup)
+        bot.send_message(message.chat.id,
+                         text="Где будем искать?",
+                         reply_markup=set_buttonz("Платные базы", "Интернет"))
 
     elif message.text == "Платные базы" or message.text == "Интернет":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("Искать материалы")
-        button2 = types.KeyboardButton("Как же надоело!!1!")
-        markup.add(button1, button2)
         bot.send_message(message.chat.id, text="Несколько часов спустя...")
-        bot.send_message(message.chat.id, text="Ты почти у цели!", reply_markup=markup)
+        bot.send_message(message.chat.id,
+                         text="Ты почти у цели!",
+                         reply_markup=set_buttonz("Искать материалы", "Как же надоело!!1!"))
 
     elif message.text == "Как же надоело!!1!":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("DeScienz Platform")
-        markup.add(button1)
         bot.send_message(message.chat.id,
                          text="В красках рассказывая другу о том, что "
                               "толкового ничего не найти, ты натыкаешься на...",
-                         reply_markup=markup)
+                         reply_markup=set_buttonz("DeScienz Platform"))
 
     elif message.text == "DeScienz Platform":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("Пройти мимо")
-        button2 = types.KeyboardButton("Опа! Что это?")
-        markup.add(button1, button2)
-        bot.send_message(message.chat.id, text="Хм...", reply_markup=markup)
+        bot.send_message(message.chat.id,
+                         text="Хм...",
+                         reply_markup=set_buttonz("Пройти мимо", "Опа! Что это?"))
 
     elif message.text == "Опа! Что это?":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("Стать супер-пупер-учёным")
-        markup.add(button1)
         bot.send_message(message.chat.id,
                          text="Это "
                               "супер-пупер "
                               "платформа!",
-                         reply_markup=markup)
+                         reply_markup=set_buttonz("Стать супер-пупер-учёным"))
 
     elif message.text == "Стать супер-пупер-учёным":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("Главное - не останавливаться на достигнутом")
-        markup.add(button1)
         bot.send_message(message.chat.id,
                          text="Ура! Теперь у тебя есть вся нужная информация "
                          "и ты пишешь статью, которую тут же публикуют!",
-                         reply_markup=markup)
+                         reply_markup=set_buttonz("Главное - не останавливаться на достигнутом"))
 
     elif message.text == "Главное - не останавливаться на достигнутом":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("Надо написать статью...")
-        markup.add(button1)
         bot.send_message(message.chat.id, text="Ты к успеху шел...")
         bot.send_message(message.chat.id, text="И дошел!")
-        bot.send_message(message.chat.id, text="Поздравляем!", reply_markup=markup)
+        bot.send_message(message.chat.id,
+                         text="Поздравляем! Публикуйся и да публикуем будешь!",
+                         reply_markup=set_buttonz("Надо написать статью..."))
 
     else:
-        bot.send_message(message.chat.id, text="Я тебя не понимаю, человек...")
+        bot.send_message(message.chat.id, text="Человек, я тебя не понимаю 🤨")
 
 
 bot.polling(none_stop=True)
